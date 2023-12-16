63afe605f1ece2c8bc52cffbf80ee3908ac21b3a
mustaqinIshak <you@example.com>
2023-12-16 13:14:55 +0800

    add sql syntax in task service

--- server/services/taskService.js
+++ server/services/taskService.js
@@ -5,8 +5,7 @@ const getAllTask = async (page = 1, listPerPage = 10) => {
   try {
     const offset = helper.getOffset(page, listPerPage)
     const row = await db.query(`
-      SELECT id, task_name, task_description, due_date 
-      FROM task_information task_infomation LIMIT ${offset},${listPerPage}
+      SELECT * FROM task_information LIMIT ${offset},${listPerPage}
     `)
 
     const data = helper.emptyOrRows(row)
@@ -20,25 +19,56 @@ const getAllTask = async (page = 1, listPerPage = 10) => {
   }
 };
   
-const getOneTask = () => {
+const getOneTask = async (taskId) => {
+  try {
+    const data = await db.query(`
+      SELECT * FROM task_information WHERE id = ${taskId}
+    `)
+
+    return {
+      data
+    }
+  } catch (error) {
+    throw error
+  }
+};
+
+const getSearchTask = async (key) => {
+  try {
+    const data = db.query(`
+      SELECT * FROM task_information WHERE task_name LIKE ${key} OR task_description LIKE ${key} OR due_date LIKE ${key}  
+    `)
+
+    return {
+      data
+    }
+  } catch (error) {
+    throw error
+  }
+}
+
+const createNewTask = async (payload) => {
+  try {
+    const insertTask = db.query(`
+
+    `)
+  } catch (error) {
+    throw error
+  }
+};
+
+const updateOneTask = (taskId, payload) => {
   return;
 };
 
-const createNewTask = () => {
-  return;
-};
-
-const updateOneTask = () => {
-  return;
-};
-
-const deleteOneTask = () => {
+const deleteOneTask = (taskId, payload) => {
   return;
 };
 
 module.exports = {
   getAllTask,
   getOneTask,
+  getSearchTask,
   createNewTask,
   updateOneTask,
   deleteOneTask,
